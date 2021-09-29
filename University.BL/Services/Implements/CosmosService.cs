8e6f2cd9f940926de3a9d8eb7ac2b2ab73de6a8a
David Santafe <santafedavid01@gmail.com>
2021-09-28 20:46:41 -0500

    CLASE 2021-09-28

---
+++ University.BL/Services/Implements/CosmosService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace University.BL.Services.Implements
+{
+    public class CosmosService
+    {
+        private static CosmosClient _cosmosClient;
+
+        public CosmosService(string cosmosConfigKey)
+        {
+            _cosmosClient = new CosmosClient(cosmosConfigKey);
+        }
+
+        public async Task<int> Insert(string database,
+            string container,
+            dynamic entity)
+        {
+            entity.Id = Guid.NewGuid().ToString();
+            var _container = _cosmosClient.GetDatabase(database).GetContainer(container);
+            var result = await _container.CreateItemAsync(entity);
+            return (int)result.StatusCode;
+        }
+
+        public async Task<int> Update(string database,
+            string container,
+            dynamic entity)
+        {
+            var _container = _cosmosClient.GetDatabase(database).GetContainer(container);
+            var result = await _container.ReplaceItemAsync(entity, entity.Id);
+            return (int)result.StatusCode;
+        }
+
+        public async Task<int> Delete(string database,
+            string container,
+            string id)
+        {
+            var _container = _cosmosClient.GetDatabase(database).GetContainer(container);
+            var result = await _container.DeleteItemAsync<object>(id, new PartitionKey(id));
+            return (int)result.StatusCode;
+        }
+
+        public async Task<IEnumerable<T>> GetAll<T>(string database,
+            string container)
+        {
+            var data = new List<T>();
+            var _container = _cosmosClient.GetDatabase(database).GetContainer(container);
+            var query = _container.GetItemLinqQueryable<T>().ToFeedIterator();
+
+            while (query.HasMoreResults)
+                foreach (var item in await query.ReadNextAsync())
+                    data.Add(item);
+
+            return data;
+        }
+
+        public async Task<T> GetById<T>(string database,
+            string container,
+            string id)
+        {            
+            var _container = _cosmosClient.GetDatabase(database).GetContainer(container);
+            var query = await _container.ReadItemAsync<T>(id, new PartitionKey(id));            
+
+            return query.Resource;
+        }
+    }
+}
--- University.Web/Controllers/StudentsController.cs
+++ University.Web/Controllers/StudentsController.cs
@@ -43,5 +43,65 @@ namespace University.Web.Controllers
 
             return View(studentDTO);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            //api/Students/GetStudent/{id}
+            var responseDTO = await apiService.RequestAPI<StudentOutputDTO>("https://university-api.azurewebsites.net/",
+                "api/Students/GetStudent/" + id,
+                null,
+                ApiService.Method.Get,
+                false);
+
+            var student = (StudentOutputDTO)responseDTO.Data;
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(StudentOutputDTO studentDTO)
+        {
+            var responseDTO = await apiService.RequestAPI<StudentDTO>("https://university-api.azurewebsites.net/",
+                "api/Students/" + studentDTO.ID,
+                studentDTO,
+                ApiService.Method.Put,
+                false);
+
+            if (responseDTO.Code == (int)HttpStatusCode.NoContent)
+                return RedirectToAction(nameof(Index));
+
+            return View(studentDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //api/Students/GetStudent/{id}
+            var responseDTO = await apiService.RequestAPI<StudentOutputDTO>("https://university-api.azurewebsites.net/",
+                "api/Students/GetStudent/" + id,
+                null,
+                ApiService.Method.Get,
+                false);
+
+            var student = (StudentOutputDTO)responseDTO.Data;
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(StudentOutputDTO studentDTO)
+        {            
+            var responseDTO = await apiService.RequestAPI<StudentOutputDTO>("https://university-api.azurewebsites.net/",
+                "api/Students/" + studentDTO.ID,
+                null,
+                ApiService.Method.Delete,
+                false);
+
+            if (responseDTO.Code == (int)HttpStatusCode.NoContent)
+                return RedirectToAction(nameof(Index));
+
+            return View(studentDTO);
+        }
     }
 }
